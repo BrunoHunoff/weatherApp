dcda4400e3a51cf85adcf976f40a5e972cd37aaa
BrunoHunoff <brunohunoff@gmail.com>
2024-01-07 12:29:16 -0300

    Primeira versão

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const apiKey = "fa705d2cc4df449f7e9253cdb11145bb";
+const apiCountryURL = "https://flagsapi.com/BR/flat/64.png";
+const iconURL = "http://openweathermap.org/img/wn/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector('#weather-data');
+// Funções
+const getWeatherData = async(city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    if(data.cod == 200) {
+        return data;
+    }
+    else if(data.cod == '404' || data.cod == '400') {
+        throw new Error(`Cidade não encontrada ${city}`);
+    }
+
+};
+
+const showWeatherData = async(city) => {
+    let weatherData = await getWeatherData(city);
+
+    console.log(weatherData);
+
+    cityElement.innerHTML = weatherData.name;
+    tempElement.innerHTML = parseInt(weatherData.main.temp);
+    descElement.innerHTML = weatherData.weather[0].description;
+    weatherIconElement.src = iconURL + weatherData.weather[0].icon + '.png';
+    countryElement.src = apiCountryURL.replace('BR', weatherData.sys.country);
+    humidityElement.innerHTML = weatherData.main.humidity;
+    windElement.innerHTML = weatherData.wind.speed;    
+  
+    weatherContainer.classList.remove("hide");
+};
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+        const city = cityInput.value;
+
+    showWeatherData(city);
+
+})
+
+cityInput.addEventListener("keyup", (a) => {
+    if (a.code == "Enter") {
+        const city = a.target.value;
+
+        showWeatherData(city);
+    }
+});
